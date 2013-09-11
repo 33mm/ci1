7f552e4d17b0ae9f0bdc82182f11e7239f97b9ad
MM <mm@greeneggmedia.com>
2013-09-11 09:10:44 -0700

    Updates .gitignore to ignore database.php Adds database_default.php file Removes database.php

--- application/config/database_default.php
+++ application/config/database_default.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'blue';
-$db['default']['database'] = 'ci1';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'CI1';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
