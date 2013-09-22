55c25ebdbddf04f3549c186570c489cbaba1fb33
mm <michaelmaleski@gmail.com>
2013-09-22 10:31:10 -0700

    Adds tools/spark and bootstrap Not really sure what these do but we will see

--- application/views/home.php
+++ application/views/home.php
@@ -8,6 +8,22 @@
 
 <body>
 Content of the document......
+
+<table border="1">
+	<tr>
+		<th>Header 1</th>
+		<th>Header 2</th>
+	</tr>
+	<tr>
+		<td><a href="http://localhost/ci/">My Code Ignighter ci1</a></td>
+		<td><a href="http://www.w3schools.com">This is a link</a></td>
+	</tr>
+	<tr>
+		<td><a href="http://www.w3schools.com">This is a link</a></td>
+		<td><a href="http://www.w3schools.com">This is a link</a></td>
+	</tr>
+</table>
+
 	<div id="container">
 		<p>view loaded</p>
 		<pre>
