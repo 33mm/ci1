3374ddcee3a0c15c7e3ec06b9e0c214936682b38
mm <michaelmaleski@gmail.com>
2013-09-22 10:54:15 -0700

    Trys to add in bootstrap... but I have no idea what I am doing

--- application/views/home.php
+++ application/views/home.php
@@ -4,18 +4,22 @@
 <head>
 <meta charset="UTF-8">
 <title>Title of the document</title>
+
+
+<link href="<?php echo site_url('assets/bootstrap/css/bootstrap.css'); ?>" >
 </head>
 
 <body>
 Content of the document......
 
-<table border="1">
+<!-- <table border="1"> -->
+<table class="table" >
 	<tr>
 		<th>Header 1</th>
 		<th>Header 2</th>
 	</tr>
 	<tr>
-		<td><a href="http://localhost/ci/">My Code Ignighter ci1</a></td>
+		<td><a href="http://localhost/ci/ci">My Code Ignighter ci1</a></td>
 		<td><a href="http://www.w3schools.com">This is a link</a></td>
 	</tr>
 	<tr>
