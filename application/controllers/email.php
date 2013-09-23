6bbe3dd809ef35baeb5ca22bf9443c87272a75a4
mm <michaelmaleski@gmail.com>
2013-09-23 00:46:48 -0700

    Day 3 and 4 complete sending emails and adding a form with form helper! All working! Bootstrap also working =)

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -3,38 +3,49 @@
 /**
 * 
 */
-class Email extends CI_Controller
-{
+class Email extends CI_Controller {
 	
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 	}
 
-	function index()
-	{
-
-		$this->load->library('email');
-		$this->email->set_newline("\r\n");
-
-		$this->email->from("mm@greeneggmedia.com", "M M");
-		$this->email->to("mm@greeneggmedia.com");
-		$this->email->subject("ci email test");
-		$this->email->message("its working");
-
-		$path = $this->config->item('server_root');
-		$file = $path . '/ci/ci/assets/email_at/test.txt';
-
-		$this->email->attach($file);
-		
-
-		if($this->email->send()){
-			echo "Email sent";
-		}
-		else{
-			show_error($this->email->print_debugger());
-		}
-
+	function index(){
+		$this->load->view('newsletter');
 	}
 
+	function send() {
+		// echo "send meth";
+		// die();
+
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('name', 'Name', 'trim|required');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		if ($this->form_validation->run()==FALSE) {
+			$this->load->view('newsletter');
+		}
+		else{
+			$name = $this->input->post('name');
+			$email = $this->input->post('email');
+
+			$this->load->library('email');
+			$this->email->set_newline("\r\n");
+
+			$this->email->from("mm@greeneggmedia.com", "M M");
+			$this->email->to("$email");
+			$this->email->subject("ci email test");
+			$this->email->message("its working");
+
+			$path = $this->config->item('server_root');
+			$file = $path . '/ci/ci/assets/email_at/test.txt';
+
+			$this->email->attach($file);
+
+			if($this->email->send()){
+				echo "Email sent";
+			}
+			else{
+				show_error($this->email->print_debugger());
+			}
+		}
+	}
 }
