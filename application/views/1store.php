0fc52404966669aa70cf1e952e4ac1786433d502
MM <mm@greeneggmedia.com>
2013-09-24 22:21:11 -0700

    Trying to override foxycart css

--- application/views/1store.php
+++ application/views/1store.php
@@ -6,6 +6,9 @@
 	
 	<!-- BEGIN FOXYCART FILES 1store-->
 	<link rel="stylesheet" href="//cdn.foxycart.com/static/scripts/colorbox/1.3.23/style1_fc/colorbox.css?ver=1" type="text/css" media="screen" charset="utf-8" />
+	<!-- Override FoxyCart CSS with your own stylesheet: -->
+	<link rel="stylesheet" type="text/css" href="<?=base_url();?>assets/bootstrap/css/bootstrap.css">
+	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
 	<script src="//cdn.foxycart.com/1store/foxycart.colorbox.js?ver=2" type="text/javascript" charset="utf-8"></script>
 	<!-- END FOXYCART FILES -->
